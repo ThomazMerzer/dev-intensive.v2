801bfeb67738b5a8ef0970d8b8bf8c48022c2f5f
ThomazMerzer <thomazmerzer@gmail.com>
2019-08-03 05:54:24 +0500

    Выполнен и проверен plural

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -34,3 +34,25 @@ fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
 
     return this
 }
+
+fun TimeUnits.plural(value: Int): String {
+    var tempStr = ""
+
+    var one = arrayOf(1, 21, 31, 41, 51, 61, 71, 81, 91, 101, 121, 131, 141, 151, 161, 171, 181, 191, 201,
+        221, 231, 241, 251, 261, 271, 281, 291, 301, 321, 331, 341, 351, 361)
+    val twoToFour = arrayOf(2, 3, 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54, 222)
+    val five = arrayOf(5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 25, 26, 27,
+        28, 29, 30, 35, 36, 37, 38, 39, 40, 45, 46, 47, 48, 49, 50, 55, 56, 57, 58, 59, 60)
+
+    val timeUnit = this.name
+
+    tempStr = when (timeUnit) {
+        "SECOND" -> if (value in one) "секунду" else if (value in twoToFour) "секунды" else "секунд"
+        "MINUTE" -> if (value in one) "минуту" else if (value in twoToFour) "минуты" else "минут"
+        "HOUR" -> if (value in one) "час" else if (value in twoToFour) "часа" else "часов"
+        "DAY" -> if (value in one) "день" else if (value in twoToFour) "дня" else "дней"
+        else -> ""
+    }
+
+    return "$value $tempStr"
+}
